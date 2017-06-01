090955dad66fbf2daa9bfa512268f914081a8912
stayasleep <virtuallyinfekted@gmail.com>
2017-05-31 17:52:19 -0700

    file path set

--- data.php
+++ data.php
@@ -17,19 +17,19 @@ $output = [
 switch($action){
 	case 'readAll':
 		//include the php file 'read.php'
-        include('dataApi/read.php');
+        include('./dataApi/read.php');
 		break;
 	case 'insert':
 		//include the php file insert.php
-        include('dataApi/insert.php');
+        include('./dataApi/insert.php');
 		break;
 	case 'delete':
 		//include the php file delete.php
-        include('dataApi/delete.php');
+        include('./dataApi/delete.php');
 		break;
 	case 'update':
 		//include the update.php file
-        include('dataApi/update.php');
+        include('./dataApi/update.php');
 		break;
 }
 //convert the $output variable to json, store the result in $outputJSON
